b8a64ec8f9605bef370743e26a8ec9b6f1c72b5b
110111000 <113594474+110111000@users.noreply.github.com>
2022-09-15 18:48:24 +0800

    Create hello ver2 .cpp

---
+++ hello ver2 .cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    
+    
+    printf( "hello 林\n\n" );
+    printf( "my height is %3d cm \n\n",175 );
+    printf( "my height is %d m %4d cm \n\n",1,75 );
+    printf( "my height is %d m %5d cm \n",1,75 );
+    
+    
+}
