6a6164383cdb4311d6e2f218aae75e70e98eb258
110111000 <113594474+110111000@users.noreply.github.com>
2022-09-15 18:48:56 +0800

    Create bye.cpp

---
+++ bye.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    printf( "hello \n "  );
+    
+    cout << "hello selena" ;
+    
+    cout << "my height is " << 1 << "m " << 70.5 << "cm";
+    
+    return 0;
+}
