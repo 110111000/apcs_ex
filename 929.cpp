e31fd5944fc520f380179257b2a2eb9e3145eaed
110111000 <113594474+110111000@users.noreply.github.com>
2022-09-29 17:46:47 +0800

    Create 929.cpp

---
+++ 929.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    int a, b, product;
+    a = 20;
+    b = 7;
+    // Calculating product
+    product = a * b;
+    printf("Product = %d", product);
+    return 0 ;
+}
