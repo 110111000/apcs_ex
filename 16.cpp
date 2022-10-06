1cc4715364f1c0aeaefba3155e0eddda693ea080
110111000 <113594474+110111000@users.noreply.github.com>
2022-10-06 17:48:22 +0800

    Create 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int
+main ()
+{
+  int a, b, temp;
+  a = 55;
+  b = 55;
+
+  if (a >= b)
+    {
+      printf ("a >= b \n");
+    }
+  else
+    {
+      printf (" a < b \n");
+    }
+
+  cout << "Hello World";
+
+  return 0;
+}
