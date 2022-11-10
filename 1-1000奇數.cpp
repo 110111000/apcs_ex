ffa12175fc849a2ae421d16accd160269045d444
110111000 <113594474+110111000@users.noreply.github.com>
2022-11-10 19:09:55 +0800

    Create 1-1000奇數.cpp

---
+++ 1-1000奇數.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+
+  int i,sum = 0  ;
+  
+  for (i = 1; i < 1000;i+=2)
+    {
+        sum = sum + i;
+	  printf ("%d %d \n", i,sum);
+	  
+	 
+	  
+	} 
+      
+    
+
+
+  return 0;
+}
