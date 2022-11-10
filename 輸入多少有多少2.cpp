130eadef76bde2ca01c9b9b7dba52e33dec7e099
110111000 <113594474+110111000@users.noreply.github.com>
2022-11-10 19:54:18 +0800

    Create 輸入多少有多少2.cpp

---
+++ 輸入多少有多少2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int main ()
+{
+    int a, i, j;
+    scanf("%d", &a);
+    for(i = 1;i<=a; i++){
+        for(j = n -i ;j >0; j--){
+            printf("*");
+        }
+        printf("\n");
+            
+    }
+    
+
+    
+
+  
+      
+    
+
+  return 0;
+}
