8f262bb1b6ec6361b51120037d64450f7d30dfae
110111000 <113594474+110111000@users.noreply.github.com>
2022-11-17 19:16:52 +0800

    Create 矩形面積.cpp

---
+++ 矩形面積.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int  multiply(int a, int b){
+    return a*b;}
+int main(){
+    int a,b;
+    printf("輸入長和寬 \n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("矩形面積 = %d\n",multiply(a,b));
+    
+    
+    cout<<"Hello World";
+    
+    return 0; 
+}
