c5fef0c70f16ffd8e5c638b00808fec3053d95a1
110111000 <113594474+110111000@users.noreply.github.com>
2022-12-29 19:22:10 +0800

    Update b513.cpp

--- b513.cpp
+++ b513.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 
 using namespace std;
-
-int main()
-{
-int a,b;
-scanf("%d",&a); 
-for(b=2;b<a;b++){
+void is_prime(int a,int b){
+    for(b=2;b<a;b++){
         if(a % b == 0){
         printf(" N \n");
        break;
         } 
-}
-        if(b == a ){ 
+    }
+    if(b == a ){ 
         printf(" Y \n"); 
-        }
+    };
+}
+
+int main()
+{
+int i,n,a,b;
+scanf("%d",&n);
+for( i=0;i<n;i++){
+    scanf("%d",&a); 
+    is_prime(a,b);
+    
+}
 return 0;
 }
 
- 
